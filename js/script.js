6e149dd15b09b8fe354274cf4ad61602e4883801
unknown <yeshratzerinyera@gmail.com>
2022-10-17 23:45:03 +0600

    Details Added

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ const showCategories = categories => {
         const li = document.createElement('li');
         li.classList.add('nav-item');
         li.innerHTML = `
-        <a class="nav-link text-secondary text-semibold remove-class" aria-current="page" href="#"
+        <a class="nav-link text-secondary text-semibold" aria-current="page" href="#"
         onclick="loadCategory('${news.category_id}','${news.category_name}')">${news.category_name}</a>
         `;
         newsContainer.appendChild(li);
@@ -29,6 +29,7 @@ const loadCategory = (id, categoryName) => {
         .then(res => res.json())
         .then(data => showCategory(data.data, categoryName))
         .catch(error => console.log(error))
+    toogleSpinner(true);
 };
 
 // Show Category
@@ -80,8 +81,7 @@ const showCategory = (category, categoryName) => {
                                             <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
                                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                         </div>
-                                        <div class="modal-body">
-                                            ...
+                                        <div class="modal-body" id="modal-body">
                                         </div>
                                         <div class="modal-footer">
                                             <button type="button" class="btn btn-info text-white fw-semibold" data-bs-dismiss="modal">Close</button>
@@ -90,7 +90,7 @@ const showCategory = (category, categoryName) => {
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </div> 
                     </div>
                 </div>
             </div>
@@ -100,43 +100,52 @@ const showCategory = (category, categoryName) => {
     toogleSpinner(false);
 };
 
-// Category Select Event
-// document.getElementById('news').addEventListener('click', function (event) {
-//     toogleSpinner(true);
-//     const categories = document.getElementsByClassName('remove-class');
-//     for (const category of categories) {
-//         category.classList.remove('bg-info', 'text-white', 'rounded');
-//     }
-//     event.target.classList.add('bg-info', 'text-white', 'rounded');
-//     document.getElementById('news').classList.remove('bg-info', 'text-white', 'rounded');
-// });
 
 // Toogle Spinner Function
-// const toogleSpinner = isLoading => {
-//     const spinner = document.getElementById('spinner');
-//     if (isLoading) {
-//         spinner.classList.remove('d-none');
-//     }
-//     else {
-//         spinner.classList.add('d-none');
-//     }
-// };
+const toogleSpinner = isLoading => {
+    const spinner = document.getElementById('spinner');
+    if (isLoading) {
+        spinner.classList.remove('d-none');
+    }
+    else {
+        spinner.classList.add('d-none');
+    }
+};
 
+// Load Category Details
+const loadCategoryDetails = id => {
+    fetch(`https://openapi.programming-hero.com/api/news/${id}`)
+        .then(res => res.json())
+        .then(data => showCategoryDetails(data.data[0]))
+        .catch(error => console.log(error))
+};
 
-// // Load Category Details
-// const loadCategoryDetails = id => {
-//     fetch(`https://openapi.programming-hero.com/api/news/${id}`)
-//         .then(res => res.json())
-//         .then(data => showCategoryDetails(data.data[0]))
-//         .catch(error => console.log(error))
-// };
-
-// // Show Category Details
-// const showCategoryDetails = category => {
-//     const id = category.rating;
-//     console.log(id);
-// };
-
-// Load Categories Function Call
-// loadCategories();
+// Show Category Details
+const showCategoryDetails = category => {
+    const title = document.getElementById('exampleModalLabel');
+    title.innerText = `${category.title ? category.title : 'No Title'}`;
+    const body = document.getElementById('modal-body');
+    body.innerHTML = `
+        <img src="${category.image_url}" class="w-100" alt="Image of news">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-between mt-2">
+            <div class="d-flex align-items-center justify-content-center
+                mt-4">
+                <img src="${category.author.img}" class="rounded-circle me-2" alt="Image of author" height="60px" width="60px">
+                <div>
+                    <div class="fs-5">${category.author.name ? category.author.name : 'Unknown Author'}</div>
+                    <div class="fs-5 text-secondary">${category.author.published_date ? category.author.published_date : 'Unknown Date'}</div>
+                </div>
+            </div>
+            <div class="d-flex align-items-center justify-content-center justify-content-between mt-4">
+                <i class="fa-solid fa-eye fs-4 text-secondary"></i>
+                <span class="fs-4 text-secondary ms-3">${category.total_view ? category.total_view : 'Unknown View'}</span>
+            </div>
+            <div class="mt-4">
+                <div class="fs-5">${category.rating.number ? category.rating.number : 'Unknown Rating'}</div>
+                <div class="fs-5 text-secondary">${category.rating.badge ? category.rating.badge : 'Unknown Badge'}</div>
+            </div>
+        </div>
+        <p class="text-secondary mt-4 fs-5">${category.details ? category.details : 'No Details'}</p>
+    `;
+};
 
